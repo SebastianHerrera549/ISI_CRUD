9effaeda42a40127d78c099d912cb4823baf063a
sebastian.herrera <sebastian.herrera.molina@hotmail.com>
2023-08-11 17:30:30 -0500

    scripts_SQL dos

--- ISI_CRUD/VistaAnalisis.sql
+++
@@ -1,6 +0,0 @@
-CREATE VIEW [dbo].[Análisis de precio] AS
-SELECT p.ProductName AS [Nombre del producto],
-       p.UnitPrice AS [Precio unitario],
-       AVG(p.UnitPrice) OVER() AS [Precio promedio de todos los productos],
-       AVG(p.UnitPrice) OVER() - p.UnitPrice AS [Diferencia entre el precio promedio y el precio del producto]
-FROM Products p;
